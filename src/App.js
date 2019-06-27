bf745b4098ebfa6f381c4fbbf790028aa1e8fb7a
AmadorZcv <lucasamadorzcv@gmail.com>
2019-06-27 13:54:59 -0300

    Eixo x e eixo y ok

--- src/App.js
+++ src/App.js
@@ -1,28 +1,124 @@
 import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch from './sketch';
-import logo from './logo.svg';
 import './App.css';
-
+import { Form } from 'react-bootstrap';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { color: [Math.random() * 255, Math.random() * 255, Math.random() * 255] };
+    this.state = {
+      color: [Math.random() * 255, Math.random() * 255, Math.random() * 255],
+      x1: 0,
+      x2: 800,
+      y1: 0,
+      y2: 800,
+      numX: 10,
+      numY: 10,
+      x: false,
+      y: false
+    };
     this.randomColor = this.randomColor.bind(this);
   }
   randomColor() {
     this.setState({ color: [Math.random() * 255, Math.random() * 255, Math.random() * 255] });
   }
+
+  //X
+
+  onChangeX = (e) => {
+    let { checked } = e.target
+    this.setState({ x: checked })
+  }
+  onChangeX1 = (e) => {
+    let { value } = e.target
+    value = Number(value)
+    this.setState({ x1: value })
+  }
+  onChangeX2 = (e) => {
+    let { value } = e.target
+    value = Number(value)
+    this.setState({ x2: value })
+  }
+  onChangeNumX = (e) => {
+    let { value } = e.target
+    value = Number(value)
+    this.setState({ numX: value })
+  }
+
+  //Y
+
+  onChangeY = (e) => {
+    let { checked } = e.target
+    this.setState({ y: checked })
+  }
+  onChangeY1 = (e) => {
+    let { value } = e.target
+    value = Number(value)
+    this.setState({ y1: value })
+  }
+  onChangeY2 = (e) => {
+    let { value } = e.target
+    value = Number(value)
+    this.setState({ y2: value })
+  }
+  onChangeNumY = (e) => {
+    let { value } = e.target
+    value = Number(value)
+    this.setState({ numY: value })
+  }
+
+
   render() {
-    return (<div className="App">
-      <header className="App-header">
-        <button onClick={this.randomColor}>Random Color</button>
-        <P5Wrapper sketch={sketch} color={this.state.color}></P5Wrapper>
-      </header>
+    const { x1, x2, numX, x, y, numY, y1, y2 } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-control">
+            <Form>
+              <Form.Group controlId="x1">
+                <Form.Label>X inicial</Form.Label>
+                <Form.Control type="number" placeholder="X1" value={this.state.x1} onChange={(text) => this.onChangeX1(text)} />
+              </Form.Group>
+
+              <Form.Group controlId="x2">
+                <Form.Label>X final</Form.Label>
+                <Form.Control type="number" placeholder="X2" value={this.state.x2} onChange={(text) => this.onChangeX2(text)} />
+              </Form.Group>
+              <Form.Group controlId="numX">
+                <Form.Label>Numero de feixes</Form.Label>
+                <Form.Control type="number" placeholder="numX" value={this.state.numX} onChange={(text) => this.onChangeNumX(text)} />
+              </Form.Group>
+              <Form.Group controlId="formBasicChecbox">
+                <Form.Check checked={this.state.x} type="checkbox" label="Eixo das Abcissas(X)" onChange={(e) => this.onChangeX(e)} />
+              </Form.Group>
+            </Form>
+            <Form>
+              <Form.Group controlId="x1">
+                <Form.Label>Y inicial</Form.Label>
+                <Form.Control type="number" placeholder="Y1" value={this.state.y1} onChange={(text) => this.onChangeY1(text)} />
+              </Form.Group>
+
+              <Form.Group controlId="x2">
+                <Form.Label>Y final</Form.Label>
+                <Form.Control type="number" placeholder="Y2" value={this.state.y2} onChange={(text) => this.onChangeY2(text)} />
+              </Form.Group>
+              <Form.Group controlId="numX">
+                <Form.Label>Numero de feixes</Form.Label>
+                <Form.Control type="number" placeholder="numX" value={this.state.numY} onChange={(text) => this.onChangeNumY(text)} />
+              </Form.Group>
+              <Form.Group controlId="formBasicChecbox">
+                <Form.Check checked={this.state.y} type="checkbox" label="Eixo das Ordenadas(Y)" onChange={(e) => this.onChangeY(e)} />
+              </Form.Group>
+            </Form>
+          </div>
+          <P5Wrapper sketch={sketch} color={this.state.color}
+            x1={x1} x2={x2} numX={numX} x={x}
+            y1={y1} y2={y2} numY={numY} y={y}></P5Wrapper>
+        </header>
 
 
-    </div>)
+      </div>)
   }
 }
 
--- src/sketch.js
+++ src/sketch.js
@@ -1,26 +1,66 @@
 export default function sketch(p) {
   let canvas;
+  let x1 = 0;
+  let x2 = 800;
+  let numX = 10;
+  let x = false
+  let y1 = 0;
+  let y2 = 800;
+  let numY = 10;
+  let y = false
 
-  let shouldDraw = false
 
-
+  function feixeX(x) {
+    p.line(x, 0, x, 800)
+  }
+  function feixeY(y) {
+    p.line(0, y, 800, y)
+  }
+  function feixesY(y1, y2, num) {
+    const espaço = y2 - y1;
+    const espaçoEntre = Math.round(espaço / num);
+    feixeY(y1)
+    while (y1 < y2) {
+      y1 = y1 + espaçoEntre;
+      feixeY(y1)
+    }
+    //feixeX(x2)
+  }
+  function feixesX(x1, x2, num) {
+    const espaço = x2 - x1;
+    const espaçoEntre = Math.round(espaço / num);
+    feixeX(x1)
+    while (x1 < x2) {
+      x1 = x1 + espaçoEntre;
+      feixeX(x1)
+    }
+    feixeX(x2)
+  }
   p.setup = () => {
-    canvas = p.createCanvas(500, 500);
-    p.noStroke();
+    canvas = p.createCanvas(800, 800);
   }
 
   p.draw = () => {
     p.background('white');
-    if (shouldDraw) {
-
-      p.ellipse(150, 100, 100, 100);
+    if (x) {
+      feixesX(x1, x2, numX)
+    }
+    if (y) {
+      feixesY(y1, y2, numY)
     }
   }
 
-  p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
+  p.myCustomRedrawAccordingToNewPropsHandler = (props) => {
     if (canvas) {//Make sure the canvas has been created
-      p.fill(newProps.color)
-      shouldDraw = !shouldDraw
+      x1 = props.x1
+      x2 = props.x2
+      numX = props.numX
+      x = props.x
+
+      y1 = props.y1
+      y2 = props.y2
+      numY = props.numY
+      y = props.y
     }
   }
 }
